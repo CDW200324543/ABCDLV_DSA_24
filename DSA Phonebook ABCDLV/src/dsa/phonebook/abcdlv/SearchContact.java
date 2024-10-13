f55387239ff74c7fac0b802270c31a477b193b19
CDW200324543 <chanelle.dewaal@gmail.com>
2024-10-13 18:08:05 +0200

    Update SearchContact.java

--- DSA Phonebook ABCDLV/src/dsa/phonebook/abcdlv/SearchContact.java
+++ DSA Phonebook ABCDLV/src/dsa/phonebook/abcdlv/SearchContact.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package PhoneBook;
+package dsa.phonebook.abcdlv;
 
 /**
  *
- * @author chane
+ * @author chanelle de waal
  */
 
 import javax.swing.table.DefaultTableModel;
